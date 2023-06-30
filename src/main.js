0a165c52b1b7b5dba1170ecc25abd7edd0fbe634
tratchapong <tratchapong@gmail.com>
2023-06-30 23:00:33 +0700

    add moving object in canvas(gameArea)

--- src/main.js
+++ src/main.js
@@ -10,11 +10,15 @@ class GameArea {
     this.canvas.height = 270
     this.context = this.canvas.getContext('2d')
     this.frameNo = 0
-    // this.interval = setInterval(updateGameArea, 20)
+    this.interval = setInterval(updateGameArea, 50)
   }
 
   start() {
     document.body.prepend(this.canvas) 
+  }
+
+  clear() {
+    this.context.clearRect(0,0,this.canvas.width, this.canvas.height)
   }
 }
 
@@ -37,22 +41,55 @@ class Component {
     let ctx = myGameArea.context
     ctx.fillStyle = this.color
     if(this?.type === 'text') {
-      console.log(999)
       ctx.font = this.width + " " + this.height
       ctx.fillText(this.text, this.x, this.y)
     } else {
       ctx.fillRect(this.x, this.y, this.width, this.height)
     }
+  }
 
+  newPos() {
+    this.gravitySpeed += this.gravity
+    this.x += this.speedX
+    this.y += this.speedY + this.gravitySpeed
+    this.hitBottom()
+  }
+
+  hitBottom() {
+    let rockbottom = myGameArea.canvas.height - this.height
+    if( this.y > rockbottom) {
+      this.y = rockbottom
+      this.gravitySpeed = 0
+    }
   }
 }
+
+let title = new Component("30px", "Consolas", "violet", 280, 40, "text")
+title.text = "Codecamp"
+let actor = new Component(30, 30, "red", 10, 120)
+let direction = 1
 
 function startGame() {
   myGameArea = new GameArea()
   myGameArea.start()
-  let actor = new Component(30, 30, "red", 10, 120)
   actor.update()
-  let title = new Component("30px", "Consolas", "violet", 280, 40, "text")
-  title.text = "Codecamp"
   title.update()
+}
+
+function updateGameArea() {
+  let ctx = myGameArea.context
+  myGameArea.clear()
+  if (title.x < -ctx.measureText(title.text).width ){
+    console.log(ctx.measureText(title.text).width)
+    title.x = myGameArea.canvas.width
+  }
+  title.x -= 5
+  title.update()
+  actor.x +=10 * direction
+  if(actor.x > myGameArea.canvas.width - actor.width)
+    direction = -1
+  if(actor.x < 0 )
+    direction = 1
+  actor.update()
+  
 }
